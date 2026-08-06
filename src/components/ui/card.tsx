46e4ed9804eb067894a92f6045adc4916e96bfa8
 alex <alex@alexs-MacBook-Air.local>
2026-08-06 08:31:15 -0700

    feat(ui): add Card primitive to replace ~20 duplicated card blocks

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { cn } from "@/utils/cn";
+
+export const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "bg-white dark:bg-gray-800 rounded border border-gray-200 dark:border-gray-700 hover:shadow transition-shadow",
+        className
+      )}
+      {...props}
+    />
+  )
+);
+Card.displayName = "Card";
+
+export const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("px-6 pt-6 pb-0", className)} {...props} />
+  )
+);
+CardHeader.displayName = "CardHeader";
+
+export const CardTitle = React.forwardRef<HTMLHeadingElement, React.HTMLAttributes<HTMLHeadingElement>>(
+  ({ className, ...props }, ref) => (
+    <h3 ref={ref} className={cn("text-lg font-semibold text-gray-900 dark:text-white", className)} {...props} />
+  )
+);
+CardTitle.displayName = "CardTitle";
+
+export const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("px-6 py-4", className)} {...props} />
+  )
+);
+CardContent.displayName = "CardContent";
+
+export const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("px-6 pb-6 pt-0", className)} {...props} />
+  )
+);
+CardFooter.displayName = "CardFooter";
